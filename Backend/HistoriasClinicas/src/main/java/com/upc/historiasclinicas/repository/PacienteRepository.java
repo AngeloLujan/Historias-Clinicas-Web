eee2b5388d3253fce378a71caa45f32c431edec8
Raul <rzcodx@gmail.com>
2023-05-02 20:35:05 -0500

    Fix búsqueda documento

--- Backend/HistoriasClinicas/src/main/java/com/upc/historiasclinicas/repository/PacienteRepository.java
+++ Backend/HistoriasClinicas/src/main/java/com/upc/historiasclinicas/repository/PacienteRepository.java
@@ -14,7 +14,7 @@ public interface PacienteRepository extends JpaRepository<Paciente,Integer> {
                                                                            @Param("numeroDocumento") String numeroDocumento);
 
     //@Query("select p from Paciente p where p.nombres = :nombres and p.apellidoPaterno = :apellidoPaterno and p.apellidoMaterno = :apellidoMaterno and p.tipoDocumento = :tipoDocumento and p.numeroDocumento = :numeroDocumento")
-    @Query("SELECT p FROM Paciente p WHERE (:nombres = ''  or p.nombres = :nombres) AND (:apellidoPaterno = ''  or p.apellidoPaterno = :apellidoPaterno) AND (:apellidoMaterno = ''  or p.apellidoMaterno = :apellidoMaterno) AND (:tipoDocumento = ''  or p.tipoDocumento = :tipoDocumento) AND (:numeroDocumento = ''  or p.numeroDocumento = :numeroDocumento)")
+    @Query("SELECT p FROM Paciente p WHERE (:nombres = ''  or p.nombres = :nombres) AND (:apellidoPaterno = ''  or p.apellidoPaterno = :apellidoPaterno) AND (:apellidoMaterno = ''  or p.apellidoMaterno = :apellidoMaterno) AND (:tipoDocumento = '' or :tipoDocumento = '-1'  or p.tipoDocumento = :tipoDocumento) AND (:numeroDocumento = ''  or p.numeroDocumento = :numeroDocumento)")
     public List<Paciente> getAllPacientesFiltro(@Param("nombres") String nombres,
                                                 @Param("apellidoPaterno") String apellidoPaterno,
                                                 @Param("apellidoMaterno") String apellidoMaterno,
